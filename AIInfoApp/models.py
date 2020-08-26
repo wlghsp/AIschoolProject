845b9187b06dc67d2b3b928c5ee477c99005925c
wlghsp <wlghsp@gmail.com>
2020-08-26 12:59:09 +0900

    db_relation_auth_1st

--- AIInfoApp/models.py
+++ AIInfoApp/models.py
@@ -1,15 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 
 class AiClass(models.Model):
     class_num = models.IntegerField()
-    lecturer = models.CharField(max_length=30)
+    lecturer = models.CharField(max_length=10)
+    class_room = models.CharField(max_length=10)
 
 
 class AiStudents(models.Model):
-    class_num = models.IntegerField()
-    name = models.CharField(max_length=30)
-    phone_num = models.CharField(max_length=15,  blank=True)
-    intro_text = models.TextField(null=True, default=' ')
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name="student")
+    participate_class = models.ForeignKey(
+        AiClass, on_delete=models.CASCADE, related_name='student')
+    name = models.CharField(max_length=10)
+    phone_num = models.CharField(max_length=15)
+
+
+class StudentPost(models.Model):
+    writer = models.ForeignKey(
+        AiStudents, on_delete=models.SET_NULL, null=True, related_name="post")
+    intro_text = models.TextField()
